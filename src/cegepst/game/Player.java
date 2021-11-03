bf12ffdd94dd97644beac3158c31d600847c6ed3
BenocxX <mathis.cote@outlook.com>
2021-11-03 16:53:46 -0400

    Added: Player constructor

--- src/cegepst/game/Player.java
+++ src/cegepst/game/Player.java
@@ -1,6 +1,7 @@
 package cegepst.game;
 
 import cegepst.engine.Buffer;
+import cegepst.engine.CollidableRepository;
 import cegepst.engine.controls.MovementController;
 import cegepst.engine.entities.ControllableEntity;
 
@@ -8,6 +9,10 @@ public class Player extends ControllableEntity {
 
     public Player(MovementController controller) {
         super(controller);
+        setDimension(30, 50);
+        teleport(250, 250);
+        setSpeed(5);
+        CollidableRepository.getInstance().registerEntity(this);
     }
 
     @Override
