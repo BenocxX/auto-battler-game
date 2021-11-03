a860a938f5ccd4d0cda0220de886dafcfa4568e2
BenocxX <mathis.cote@outlook.com>
2021-11-03 17:00:09 -0400

    Added: A zone trigger to buy station

--- src/cegepst/game/BuyStation.java
+++ src/cegepst/game/BuyStation.java
@@ -9,18 +9,33 @@ import java.awt.*;
 
 public class BuyStation extends MovableEntity {
 
+    private ZoneTrigger zoneTrigger;
     private Friend friendForSell;
+    private boolean isSelected;
 
     public BuyStation(int x, int y) {
         setDimension(30, 30);
         teleport(x, y);
+        zoneTrigger = new ZoneTrigger(x - 10, y + height + 10, 50, 50);
         friendForSell = new Friend(x + 10, y + 10);
         CollidableRepository.getInstance().registerEntity(this);
     }
 
+    public void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+
+    public boolean checkIfSelected(StaticEntity entity) {
+        return zoneTrigger.intersectWith(entity);
+    }
+
     @Override
     public void draw(Buffer buffer) {
-        buffer.drawRectangle(x, y, width, height, new Color(94, 71, 47));
+        if (isSelected) {
+            buffer.drawRectangle(x, y, width, height, new Color(137, 106, 77));
+        } else {
+            buffer.drawRectangle(x, y, width, height, new Color(94, 71, 47));
+        }
         friendForSell.draw(buffer);
     }
 }
