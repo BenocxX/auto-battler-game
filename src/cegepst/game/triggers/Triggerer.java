94a9c139e5d90a86b09d6f571fcd2252dab0a7c0
BenocxX <mathis.cote@outlook.com>
2021-11-09 21:58:11 -0500

    Comment: triggerer is a valid word

--- src/cegepst/game/triggers/Triggerer.java
+++ src/cegepst/game/triggers/Triggerer.java
@@ -1,5 +1,7 @@
 package cegepst.game.triggers;
 
+// Triggerer is a valid word
+// Source: https://ell.stackexchange.com/questions/274203/is-triggerer-correct-or-is-there-some-other-word-to-identify-the-person-who-t
 public interface Triggerer {
 
     void isTriggering(TriggerRepository repository);
