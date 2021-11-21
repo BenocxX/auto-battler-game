690877bab2235236da001a590fef17940c284023
BenocxX <mathis.cote@outlook.com>
2021-11-21 15:33:32 -0500

    Add: question for teacher

--- src/cegepst/game/eventsystem/EventSystem.java
+++ src/cegepst/game/eventsystem/EventSystem.java
@@ -7,6 +7,19 @@ import java.util.ArrayList;
 
 public class EventSystem {
 
+    /** TODO: QUESTION
+     *  J'ai créé un event system afin de pouvoir call des méthodes d'objets
+     *  suite à un déclenchement venant d'un autre classe. Ce moyen contre
+     *  l'utilisation de multiples getters/setters et évite le passage
+     *  d'objet en paramètre.
+     *
+     *  Cependant, est-ce que ça brise l'encapsulation? Étant donné que je
+     *  peux call une méthode d'un objet sans avoir besoin de son instance..?
+     *
+     *  J'ai quelques difficultés à voir si c'est correct. Genre ça me semble
+     *  correct, mais je trouve ça louche en même temps.
+     */
+
     private static EventSystem instance;
 
     private ArrayList<TriggerAreaListener> triggerAreaListeners;
