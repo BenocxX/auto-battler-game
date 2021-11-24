946e7dfd6b859220859b21f87bcc8ddeeed6b92d
BenocxX <mathis.cote@outlook.com>
2021-11-24 14:34:40 -0500

    Refactor: creation of a ButtonSystem class to remove code duplication and add flexibility around buttons

--- src/cegepst/game/displays/MainMenuDisplay.java
+++ src/cegepst/game/displays/MainMenuDisplay.java
@@ -1,37 +1,31 @@
 package cegepst.game.displays;
 
 import cegepst.engine.Buffer;
-import cegepst.engine.buttons.RoundButton;
+import cegepst.engine.buttons.ButtonSystem;
 import cegepst.engine.controls.MouseController;
 import cegepst.game.GamePad;
 import cegepst.game.entities.ButtonFactory;
 
 import java.awt.*;
-import java.util.ArrayList;
 
 public class MainMenuDisplay extends Display {
 
     private GamePad gamePad;
     private MouseController mouse;
-    private ArrayList<RoundButton> buttons;
-    private int selectedButtonIndex;
+    private ButtonSystem buttonSystem;
 
     public MainMenuDisplay(DisplayType displayType) {
         super(displayType);
         gamePad = new GamePad();
         mouse = new MouseController();
-        buttons = new ArrayList<>();
-        initializeButtons();
+        initializeButtonSystem();
     }
 
     @Override
     public int update() {
         resetStateData(gamePad);
         quitKeyCheck();
-        enterKeyCheck();
-        upDownKeyCheck();
-        mouseHoverCheck();
-        mouseClickCheck();
+        buttonSystem.update();
         gamePad.clearTypedKeys();
         updateAlreadyInDisplay();
         return currentId;
@@ -40,9 +34,7 @@ public class MainMenuDisplay extends Display {
     @Override
     public void draw(Buffer buffer) {
         buffer.setFontSize(Font.PLAIN, 20);
-        for (RoundButton button : buttons) {
-            button.draw(buffer);
-        }
+        buttonSystem.draw(buffer);
     }
 
     private void quitKeyCheck() {
@@ -51,84 +43,13 @@ public class MainMenuDisplay extends Display {
         }
     }
 
-    private void enterKeyCheck() {
-        if (gamePad.isEnterTyped()) {
-            buttons.get(selectedButtonIndex).customEvent();
-        }
-    }
-
-    private void upDownKeyCheck() {
-        upKeyPressed();
-        downKeyPressed();
-    }
-
-    private void upKeyPressed() {
-        if (gamePad.isUpTyped()) {
-            decrementSelectedButtonIndex();
-            for (RoundButton button : buttons) {
-                button.isSelected(button == buttons.get(selectedButtonIndex));
-            }
-        }
-    }
-
-    private void downKeyPressed() {
-        if (gamePad.isDownTyped()) {
-            incrementSelectedButtonIndex();
-            for (RoundButton button : buttons) {
-                button.isSelected(button == buttons.get(selectedButtonIndex));
-            }
-        }
-    }
-
-    private void decrementSelectedButtonIndex() {
-        if (selectedButtonIndex > 0) {
-            selectedButtonIndex--;
-        } else {
-            selectedButtonIndex = buttons.size() - 1;
-        }
-    }
-
-    private void incrementSelectedButtonIndex() {
-        if (selectedButtonIndex < buttons.size() - 1) {
-            selectedButtonIndex++;
-        } else {
-            selectedButtonIndex = 0;
-        }
-    }
-
-    private void mouseHoverCheck() {
-        buttonHoverCheck();
-    }
-
-    private void buttonHoverCheck() {
-        for (RoundButton button : buttons) {
-            button.checkIfHovered(mouse.getMousePosition());
-            if (button.isHovered()) {
-                buttons.get(selectedButtonIndex).isSelected(false);
-            }
-        }
-    }
-
-    private void mouseClickCheck() {
-        if (mouse.isClicked()) {
-            buttonClickCheck();
-        }
-        mouse.resetIsClicked();
-    }
-
-    private void buttonClickCheck() {
-        for (RoundButton button : buttons) {
-            if (button.isClicked(mouse.getMousePosition())) {
-                button.customEvent();
-                break;
-            }
-        }
-    }
-
-    private void initializeButtons() {
-        buttons.add(ButtonFactory.playButton(this));
-        buttons.add(ButtonFactory.optionButton(this));
-        buttons.add(ButtonFactory.quitButton(this));
-        buttons.get(0).isSelected(true);
+    private void initializeButtonSystem() {
+        buttonSystem = new ButtonSystem();
+        buttonSystem.addGamePad(gamePad);
+        buttonSystem.addMouse(mouse);
+        buttonSystem.addButton(ButtonFactory.playButton(this));
+        buttonSystem.addButton(ButtonFactory.optionButton(this));
+        buttonSystem.addButton(ButtonFactory.quitButton(this));
+        buttonSystem.getButton(0).isSelected(true);
     }
 }
