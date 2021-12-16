648b3c5dcc73bc8ecee085e47e3c6fc66090f78f
BenocxX <mathis.cote@outlook.com>
2021-12-15 21:31:44 -0500

    Added new test

--- src/cegepst/game/displays/GameDisplay.java
+++ src/cegepst/game/displays/GameDisplay.java
@@ -43,6 +43,7 @@ public class GameDisplay extends Display
     private Initializer initializer;
     private MenuSystem shopMenuSystem;
     private MenuSystem battleMenuSystem;
+    // test
     private ArrayList<ShopStation> shopStations;
     private ArrayList<TriggerArea> triggerAreas;
     private ArrayList<Line> lines;
